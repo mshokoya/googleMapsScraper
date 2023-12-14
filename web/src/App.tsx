787597e07e7f198651d7d7c908bcb4c2c55091f2
msho <mayo_s@hotmail.co.uk>
2023-12-14 01:50:09 +0000

    implimented global context & sidebar

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,14 @@
+import { Sidebar } from "./components/Sidebar"
+import { DataDisplay } from "./components/DataDisplay"
+import { useState } from "react"
+
 function App() {
+  const [selectedID, setSelectedID] = useState('')
+
   return (
     <div>
-      <div></div>
-      <div></div>
+      <Sidebar setSelectedID={setSelectedID} />
+      <DataDisplay selectedID={selectedID} />
     </div>
   )
 }
